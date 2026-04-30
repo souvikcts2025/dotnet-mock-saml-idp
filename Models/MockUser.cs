d4ec95db7ca19de83d1746b385dd01f798a9265c
Souvik Sengupta <souvik.sengupta@cognizant.com>
2026-04-30 16:45:00 +0100

    Added Controllers|Models|Properties

---
+++ Models/MockUser.cs
@@ -0,0 +1,9 @@
+namespace MockSamlIdp.Models;
+
+public class MockUser
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string DisplayName { get; set; } = string.Empty;
+    public string ObjectId { get; set; } = string.Empty;
+}
